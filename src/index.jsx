bb9a747bdc3445930d14c67aab302b5753c0a107
Masud Ahmed <masud@safesecurely.com>
2023-10-12 10:55:40 -0400

    completed emoji add button

--- src/index.jsx
+++ src/index.jsx
@@ -11,6 +11,8 @@ import IconButton from './components/IconButton';
 import EmojiPicker from "./components/EmojiPicker";
 
 import EmojiList from './components/EmojiList';
+import EmojiSticker from './components/EmojiSticker';
+
 
 const PlaceholderImage = require("../assets/images/background-image.png");
 
@@ -59,6 +61,7 @@ const [pickedEmoji, setPickedEmoji] = useState(null);
           placeholderImageSource={PlaceholderImage}
           selectedImage={selectedImage}
         />
+        {pickedEmoji !== null ? <EmojiSticker imageSize={40} stickerSource={pickedEmoji} /> : null}
       </View>
       {showAppOptions ? (
         <View />
